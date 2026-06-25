74f49657d564706d17481b80f8700cad9b3db59c
sarvesh9213 <sarvesh.pandey9213@gmail.com>
2026-06-25 13:22:28 +0530

    Update importdata.py

--- MLCODE/importdata.py
+++ MLCODE/importdata.py
@@ -1,1 +1,19 @@
+import os
+import sys
+
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+import seaborn as sns
+sns.set()
+
+import warnings
+warnings.filterwarnings("ignore")
+
+pd.set_option("display.max_rows", 500)
+pd.set_option("display.max_columns", 50)
+pd.set_option("display.width", 1000)
+
+print("Everything imported successfully!")
